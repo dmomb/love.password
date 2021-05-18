dc84fad4ab30795d5e93aec7021fb8094b3fc0b9
nini <32774559+dmomb@users.noreply.github.com>
2021-05-18 20:40:18 +0800

    Add files via upload

---
+++ yanzheng.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function checkpassword(){
+                var pwd="0327";
+                var inpwd=document.getElementById("password").value;
+                 if(pwd == inpwd){
+                      window.location.href="../lovingmaster/index.html";
+                 }
+           }
