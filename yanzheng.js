dabd0f8d4d8c1d706f387245b1fb5268eb5e1522
nini <32774559+dmomb@users.noreply.github.com>
2021-05-18 20:50:13 +0800

    Update yanzheng.js

--- yanzheng.js
+++ yanzheng.js
@@ -3,6 +3,6 @@ function checkpassword(){
                 var pwd="0327";
                 var inpwd=document.getElementById("password").value;
                  if(pwd == inpwd){
-                      window.location.href="../lovingmaster/index.html";
+                      window.location.href="https://dmomb.github.io/nini520/";
                  }
            }
