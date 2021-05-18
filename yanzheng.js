1e4dc267065eada65ca6d9651e7b1444ca962170
nini <32774559+dmomb@users.noreply.github.com>
2021-05-18 20:52:08 +0800

    Update yanzheng.js

--- yanzheng.js
+++ yanzheng.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 function checkpassword(){
-                var pwd="0327";
+                var pwd="0701";
                 var inpwd=document.getElementById("password").value;
                  if(pwd == inpwd){
                       window.location.href="https://dmomb.github.io/nini520/";
